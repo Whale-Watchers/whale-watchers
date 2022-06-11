62669d5ccc0768882ead029726e08e53dbb1a9c7
Maxwell Shick <maxwellshick@gmail.com>
2022-06-11 13:17:36 -0400

    Started the React components

---
+++ client/components/nav.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ethPrice: 0
+    }
+  }
+
+  render() {
+
+  }
+}
+
+export default Nav;
