195c703370dc47d3f43aeaf107df53e1f3d69440
Gandalf <email@email.com>
2022-06-12 14:31:02 -0500

    dataDump and testDump successfully fetched from Etherscan API

--- server/controllers/etherscanController.js
+++ server/controllers/etherscanController.js
@@ -1,6 +1,7 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const fs = require("fs");
+const mkdirp = require("mkdirp");
 const path = require("path");
 const etherscanController = {};
 
@@ -111,86 +112,132 @@ etherscanController.get20Transactions = async (req, res, next) => {
 };
 
 etherscanController.dataDump = async (req, res, next) => {
-  const whaleAddresses = {
-    "0x3b417faee9d2ff636701100891dc2755b5321cc3": "Jay-Z",
-    "0x7217bc604476859303a27f111b187526231a300c": "Mike Tyson",
-    "0xd2aff66959ee0e6f92ee02d741071ddb5084bebb": "Blau",
-    "0x3becf83939f34311b6bee143197872d877501b11": "Stephen Curry",
-    "0xe4bbcbff51e61d0d95fcc5016609ac8354b177c4": "Steve Aoki",
-    "0xc1064e3662b0718357e9050694a3bfeaabede8ab": "LaMello Ball",
-    "0x3781d92e5449b5b689fee308ded44882085b6312": "Lindsay Lohan",
-    "0xa679c6154b8d4619af9f83f0bf9a13a680e01ecf": "Mark Cuban",
-    "0xc6b0562605d35ee710138402b878ffe6f2e23807": "Beeple",
-    "0xce90a7949bb78892f159f428d0dc23a8e3584d75": "Snoop Dog",
-    "0x8d3bc45d7b30013c37c141f6ce7c981b2613efaa": "JaRule",
-    "0x0864224f3cc570ab909ebf619f7583ef4a50b826": "Serena Williams",
-    "0x0ed1e02164a2a9fad7a9f9b5b9e71694c3fad7f2": "Alexis Ohanian",
-    "0xb55eb9bd32d6ab75d7555192e7a3a7ca0bcd5738": "Mike Shinoda",
-    "0x3c6aeff92b4b35c2e1b196b57d0f8ffb56884a17": "Shaquille O'Neal",
-    "0xf6de94be96f80602d90bf29bd9e88a0e843b2eb9": "Elijah Wood",
-    "0x9114b66e4bd387eb832d1477e86ede0cf7f76115": "Seth Curry",
-    "0x35cded880959f93c415723902f91f964367a4dcd": "Joel Madden",
-    "0x7948aa99e095dbfc1971bc8d2d1173893146630e": "Reese Witherspoon",
-    "0xdbf2445e5049c04cda797dae60ac885e7d79df9d": "Jordan Belfort",
-    "0xa0eaf6b0df87132c9a28e450a43c1d906defb60b": "DJ Marshmello",
-    "0x4b1bdae6b46c2ed904581d7e4bf2b71e5f3f7072": "Timbaland",
-    "0xc86b12d850fdbbf3260a7baae862f85857aadbba": "Lil Baby",
-    "0x0394451c1238cec1e825229e692aa9e428c107d8": "Jimmy Fallon",
-    "0xbea020c3bd417f30de4d6bd05b0ed310ac586cc0": "Post Malone",
-    "0xff0bd4aa3496739d5667adc10e2b843dfab5712b": "LOgan Paul",
-    "0xd6a984153acb6c9e2d788f08c2465a1358bb89a7": "Gary Vee",
-    "0x79f261f483b7cef4f995c1f8a0f46f88450423e3": "Eminenm",
-    "0xb6aa5a1aa37a4195725cdf1576dc741d359b56bd": "Paris Hilton",
-    "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d": "Neymar",
-    "0xbbdac7ba85af15420afd1f4aa3313c3535b15cde": "Kevin Hart",
-    "0x31185f782a7c11044566d70dfcf1c8175486f451": "Gwyneth Paltrow",
-    "0xaa1b056286a66a9e6752c26776ac034c662a51d5": "Eva Longoria",
-    "0x58473e9ac681c4424ca74619281ff71801d002d6": "Dillon Francis",
-    "0x6ef962ea7e64e771d3a81bce4f95328d76d7672b": "Madonna",
-  };
+  // const whaleAddresses = {
+  //   "0x3b417faee9d2ff636701100891dc2755b5321cc3": "Jay-Z",
+  //   "0x7217bc604476859303a27f111b187526231a300c": "Mike Tyson",
+  //   "0xd2aff66959ee0e6f92ee02d741071ddb5084bebb": "Blau",
+  //   "0x3becf83939f34311b6bee143197872d877501b11": "Stephen Curry",
+  //   "0xe4bbcbff51e61d0d95fcc5016609ac8354b177c4": "Steve Aoki",
+  //   "0xc1064e3662b0718357e9050694a3bfeaabede8ab": "LaMello Ball",
+  //   "0x3781d92e5449b5b689fee308ded44882085b6312": "Lindsay Lohan",
+  //   "0xa679c6154b8d4619af9f83f0bf9a13a680e01ecf": "Mark Cuban",
+  //   "0xc6b0562605d35ee710138402b878ffe6f2e23807": "Beeple",
+  //   "0xce90a7949bb78892f159f428d0dc23a8e3584d75": "Snoop Dog",
+  //   "0x8d3bc45d7b30013c37c141f6ce7c981b2613efaa": "JaRule",
+  //   "0x0864224f3cc570ab909ebf619f7583ef4a50b826": "Serena Williams",
+  //   "0x0ed1e02164a2a9fad7a9f9b5b9e71694c3fad7f2": "Alexis Ohanian",
+  //   "0xb55eb9bd32d6ab75d7555192e7a3a7ca0bcd5738": "Mike Shinoda",
+  //   "0x3c6aeff92b4b35c2e1b196b57d0f8ffb56884a17": "Shaquille O'Neal",
+  //   "0xf6de94be96f80602d90bf29bd9e88a0e843b2eb9": "Elijah Wood",
+  //   "0x9114b66e4bd387eb832d1477e86ede0cf7f76115": "Seth Curry",
+  //   "0x35cded880959f93c415723902f91f964367a4dcd": "Joel Madden",
+  //   "0x7948aa99e095dbfc1971bc8d2d1173893146630e": "Reese Witherspoon",
+  //   "0xdbf2445e5049c04cda797dae60ac885e7d79df9d": "Jordan Belfort",
+  //   "0xa0eaf6b0df87132c9a28e450a43c1d906defb60b": "DJ Marshmello",
+  //   "0x4b1bdae6b46c2ed904581d7e4bf2b71e5f3f7072": "Timbaland",
+  //   "0xc86b12d850fdbbf3260a7baae862f85857aadbba": "Lil Baby",
+  //   "0x0394451c1238cec1e825229e692aa9e428c107d8": "Jimmy Fallon",
+  //   "0xbea020c3bd417f30de4d6bd05b0ed310ac586cc0": "Post Malone",
+  //   "0xff0bd4aa3496739d5667adc10e2b843dfab5712b": "LOgan Paul",
+  //   "0xd6a984153acb6c9e2d788f08c2465a1358bb89a7": "Gary Vee",
+  //   "0x79f261f483b7cef4f995c1f8a0f46f88450423e3": "Eminenm",
+  //   "0xb6aa5a1aa37a4195725cdf1576dc741d359b56bd": "Paris Hilton",
+  //   "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d": "Neymar",
+  //   "0xbbdac7ba85af15420afd1f4aa3313c3535b15cde": "Kevin Hart",
+  //   "0x31185f782a7c11044566d70dfcf1c8175486f451": "Gwyneth Paltrow",
+  //   "0xaa1b056286a66a9e6752c26776ac034c662a51d5": "Eva Longoria",
+  //   "0x58473e9ac681c4424ca74619281ff71801d002d6": "Dillon Francis",
+  //   "0x6ef962ea7e64e771d3a81bce4f95328d76d7672b": "Madonna",
+  // };
 
-  for (address of Object.keys(whaleAddresses)) {
-    console.log("address", address);
-    const ethData = await fetch(
-      `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=NXZTP6HAGCIJH1D9UPIGEK8BDYH5RNG2AH`
-    );
-    const ethBody = await ethData.json();
-    const erc20Data = await fetch(
-      `https://api.etherscan.io/api?module=account&action=tokentx&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=NXZTP6HAGCIJH1D9UPIGEK8BDYH5RNG2AH`
-    );
-    const erc20Body = await erc20Data.json();
-    const erc721Data = await fetch(
-      `https://api.etherscan.io/api?module=account&action=tokennfttx&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=NXZTP6HAGCIJH1D9UPIGEK8BDYH5RNG2AH`
-    );
-    const erc721Body = await erc721Data.json();
+  const { address } = req.params;
+  console.log("req.params", req.params);
+  console.log("address", address);
 
-    if (ethData.status !== 200) {
-      next({
-        message: `error processing ethData for ${address}: ERROR: ${ethData.message}`,
-      });
-    } else if (erc20Data.status !== 200) {
-      next({
-        message: `error processing erc20Data for ${address}: ERROR: ${erc20Data.message}`,
-      });
-    } else if (erc721Data.status !== 200) {
-      next({
-        message: `error processing erc721data for ${address}: ERROR: ${erc721Data.message}`,
-      });
-    }
+  // for (let address of Object.keys(whaleAddresses)) {
+  // console.log("address", address);
+  const ethData = await fetch(
+    `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=NXZTP6HAGCIJH1D9UPIGEK8BDYH5RNG2AH`
+  );
+  const ethBody = await ethData.json();
+  const erc20Data = await fetch(
+    `https://api.etherscan.io/api?module=account&action=tokentx&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=NXZTP6HAGCIJH1D9UPIGEK8BDYH5RNG2AH`
+  );
+  const erc20Body = await erc20Data.json();
+  const erc721Data = await fetch(
+    `https://api.etherscan.io/api?module=account&action=tokennfttx&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=NXZTP6HAGCIJH1D9UPIGEK8BDYH5RNG2AH`
+  );
+  const erc721Body = await erc721Data.json();
 
-    fs.appendFileSync(
-      path.resolve(__dirname, "../data/dataDump.js"),
-      JSON.stringify(ethBody.result)
-    );
-    fs.appendFileSync(
-      path.resolve(__dirname, "../data/dataDump.js"),
-      JSON.stringify(erc20Body.result)
-    );
-    fs.appendFileSync(
-      path.resolve(__dirname, "../data/dataDump.js"),
-      JSON.stringify(erc721Body.result)
-    );
+  if (ethData.status !== 200) {
+    next({
+      message: `error processing ethData for ${address}: ERROR: ${ethData.message}`,
+    });
+  } else if (erc20Data.status !== 200) {
+    next({
+      message: `error processing erc20Data for ${address}: ERROR: ${erc20Data.message}`,
+    });
+  } else if (erc721Data.status !== 200) {
+    next({
+      message: `error processing erc721data for ${address}: ERROR: ${erc721Data.message}`,
+    });
+    // }
   }
+  const output = [];
+  // let i = 1;
+
+  for (let tx of ethBody.result) {
+    output.push(tx);
+    // output[`erc20-${i}`] = tx;
+    // i++;
+  }
+  for (let tx of erc20Body.result) {
+    output.push(tx);
+    // output[`erc20-${i}`] = tx;
+    // i++;
+  }
+  for (let tx of erc721Body.result) {
+    output.push(tx);
+    // output[`erc20-${i}`] = tx;
+    // i++;
+  }
+
+  // console.log("output", output);
+
+  const writeLocation = path.resolve(__dirname, `../data/testDump.json`);
+
+  fs.appendFileSync(writeLocation, JSON.stringify(output, null, 2));
+
+  // const writeFile = async (path, content) => {
+  //   await mkdirp(path);
+  //   fs.writeFileSync(path, content);
+  // };
+
+  // writeFile(newPath, JSON.stringify(output));
+
+  // fs.writeFileSync(
+  //   path.resolve(__dirname, `./data/${whaleAddresses[address]}dataDump.js`),
+  //   JSON.stringify(output),
+  //   { flag: "wx" },
+  //   function (err) {
+  //     if (err) throw err;
+  //     console.log("It's saved!");
+  //   }
+  // );
+
+  // fs.appendFileSync(
+  //   path.resolve(__dirname, `./data/${whaleAddresses[address]}dataDump.js`),
+  //   JSON.stringify(ethBody.result)
+  // );
+  // fs.appendFileSync(
+  //   path.resolve(__dirname, `./data/${whaleAddresses[address]}dataDump.js`),
+  //   JSON.stringify(erc20Body.result)
+  // );
+  // fs.appendFileSync(
+  //   path.resolve(__dirname, `./data/${whaleAddresses[address]}dataDump.js`),
+  //   JSON.stringify(erc721Body.result)
+  // );
+
   next();
 };
 
